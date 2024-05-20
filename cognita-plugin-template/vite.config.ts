63a53551d676fcaa2c8b0b74aa8b8e33a4982558
Dax Harris <matteovh@gmail.com>
2024-05-20 14:23:07 -0400

    plugin setup

---
+++ cognita-plugin-template/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "cognita-plugin-template",
+      formats: ["cjs"],
+      fileName(format, entryName) {
+        return `${entryName}.cognita-plugin-template.js`;
+      },
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@mantine/core", "@mantine/hooks"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
